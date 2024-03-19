79a074674243405240d60c733806c3db0d66739a
Hans Oskar Trolla <146342767+hansotTLU@users.noreply.github.com>
2024-03-19 18:59:37 +0200

    7. tunni lõpp
    Andmebaas + front end'iga sidumine
    
    programmid:
    	pgAdmin 4
    	Node.js
    	Postman

--- 06_liides/salat/src/main/java/ee/tlu/salat/ToiduaineEntityController.java
+++ 06_liides/salat/src/main/java/ee/tlu/salat/ToiduaineEntityController.java
@@ -1,5 +1,6 @@
 package ee.tlu.salat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,12 +10,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class ToiduaineEntityController {
 
-    List<ToiduaineEntity> toiduained = new ArrayList<>(); // imiteerime andmebaasi
+    // Üks võimalus:
+    // @Autowired
+    // ToiduaineRepository toiduaineRepository;
+
+    // Teine võimalus:
+    ToiduaineRepository toiduaineRepository;
+    public ToiduaineEntityController(ToiduaineRepository toiduaineRepository){
+        this.toiduaineRepository = toiduaineRepository;
+    }
+
+
+    // List<ToiduaineEntity> toiduained = new ArrayList<>(); // imiteerime andmebaasi
 
     // localhost:8080/api/toiduained  ---> []
     @GetMapping("toiduained")
     public List<ToiduaineEntity> saaToiduained() {
-        return toiduained;
+        return toiduaineRepository.findAll();
     }
 
     // PostMapping'ut ei saa brauser teha, saab kasutada POSTMAN'i
@@ -27,26 +39,38 @@ public class ToiduaineEntityController {
             @PathVariable int sysivesik
     ) {
         if (valk + rasv + sysivesik > 100) {
-            return toiduained;
+            return toiduaineRepository.findAll();
         }
         ToiduaineEntity toiduaine = new ToiduaineEntity(nimi, valk, rasv, sysivesik);
-        toiduained.add(toiduaine);
-        return toiduained;
+        toiduaineRepository.save(toiduaine);
+        return toiduaineRepository.findAll();
     }
+
+    @PostMapping("toiduained")
+    public List<ToiduaineEntity> lisaToiduaine(@RequestBody ToiduaineEntity toiduaineEntity) {
+        if (toiduaineEntity.valk + toiduaineEntity.rasv + toiduaineEntity.sysivesik > 100) {
+            return toiduaineRepository.findAll();
+        }
+        // ToiduaineEntity toiduaine = new ToiduaineEntity(nimi, valk, rasv, sysivesik);
+        toiduaineRepository.save(toiduaineEntity);
+        return toiduaineRepository.findAll();
+    }
+
+
 
     // DeleteMapping'ut ei saa brauser teha, saab kasutada POSTMAN'i
     // localhost:8080/api/toiduained/0 DELETE
-    @DeleteMapping("toiduained/{index}")
+    @DeleteMapping("toiduained/{nimi}")
     public List<ToiduaineEntity> kustutaToiduaine(
-            @PathVariable int index
+            @PathVariable String nimi
     ) {
-        toiduained.remove(index);
-        return toiduained;
+        toiduaineRepository.deleteById(nimi);
+        return toiduaineRepository.findAll();
     }
 
     // PutMapping'ut ei saa brauser teha, saab kasutada POSTMAN'i
-    // localhost:8080/api/toiduained/Vorst/15/5/1
-    // localhost:8080/api/toiduained?index=0&nimi=Vorst&valk=15&rasv=5&sysivesik=1   saab tühjaks jätta mingid asjad
+    // localhost:8080/api/toiduained/Vorst/15/5/1   <-- @PathVariable'iga tehtes
+    // localhost:8080/api/toiduained?index=0&nimi=Vorst&valk=15&rasv=5&sysivesik=1  <-- @RequestParam'iga tehtes, saab tühjaks jätta mingid asjad
     @PutMapping("toiduained")
     public List<ToiduaineEntity> muudaToiduaine(
             @RequestParam int index,
@@ -56,22 +80,20 @@ public class ToiduaineEntityController {
             @RequestParam int sysivesik
     ) {
         ToiduaineEntity toiduaine = new ToiduaineEntity(nimi, valk, rasv, sysivesik);
-        toiduained.set(index, toiduaine);
-        return toiduained;
+        toiduaineRepository.save(toiduaine); // täpselt sama mis POST
+        // Hibernate kontrollib, kas on juba sellise primaarivõtmega element andmebaasis
+        return toiduaineRepository.findAll();
     }
 
-    @GetMapping("toiduained/{index}")
+    @GetMapping("toiduained/{nimi}")
     public ToiduaineEntity saaYksToiduaine(
-            @PathVariable int index
+            @PathVariable String nimi
     ) {
-        return toiduained.get(index);
+        return toiduaineRepository.findById(nimi).get();
     }
 
-
-
-// koduseks tööks näide:
     @GetMapping("toiduained-koguarv")
     public int toiduaineteKoguarv() {
-        return toiduained.size();
+        return toiduaineRepository.findAll().size();
     }
 }
